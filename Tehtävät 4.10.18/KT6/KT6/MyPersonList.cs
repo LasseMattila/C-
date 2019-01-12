73c2edd964afff374228fc08b6a5ca22b055c17c
LasseMattila <46316152+LasseMattila@users.noreply.github.com>
2019-01-12 17:00:50 +0200

    Add files via upload

---
+++ Tehtävät 4.10.18/KT6/KT6/MyPersonList.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PersonLibrary;
+
+namespace KT6
+{
+    class MyPersonList : PersonList
+
+    {
+        int Age;
+
+        public int Age1 { get => Age; set => Age = value; }
+
+        public override void Add(PersonPlus person)
+        {
+            if (Age >= 18)
+            {
+                personList.Add(person);
+            }
+            
+                      
+        }
+
+        public override PersonPlus GetAt(int index)
+        {
+            if (Age >= 20)
+            {
+                return base.GetAt(index);
+            }
+
+            else
+            {
+                return null;
+            }
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
---
+++ Tehtävät 4.10.18/KT6/KT6/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PersonLibrary;
+	
+
+namespace KT6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+        }
+    }
+}
