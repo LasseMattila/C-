0635e00f6c16d50cc4fe98e54f98bde4e99b705a
LasseMattila <46316152+LasseMattila@users.noreply.github.com>
2019-01-12 17:09:39 +0200

    Add files via upload

---
+++ Tehtävät 27.9.2018/Tehtävät 27.9.2018/KT4/MinunLuokka.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KT4
+{
+    static class MinunLuokka
+    {
+        private static string nimi;
+
+        public static string Nimi { get => nimi; set => nimi = value; }
+
+        public static string TulostaNimi()
+        {
+
+            return Nimi;
+
+        } 
+
+    }
+}
