a07bbfe4e1f09590170a30a935547a7830432c24
LasseMattila <46316152+LasseMattila@users.noreply.github.com>
2019-01-12 17:08:04 +0200

    Add files via upload

---
+++ Tehtävät 13.9.2018/Tehtävät 13.9.2018/Luokat1/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Luokat1
+{
+    class Tervehtijä
+    {
+        string tervehdys;
+        public void AsetaTervehdys(string uusiTervehdys)
+        {
+
+            tervehdys = uusiTervehdys;
+
+        }
+
+
+        public void Tervehdi()
+        {
+
+            Console.WriteLine("Tere!");
+
+        }
+
+       
+
+        public void Tervehdi(string omaTervehdys)
+        {
+
+            tervehdys = omaTervehdys;
+
+        }
+
+
+        public string HaeTervehdys()
+        {
+            return "Moro!";
+        }
+    }
+}
---
+++ Tehtävät 13.9.2018/Tehtävät 13.9.2018/Luokat1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Luokat1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tervehtijä tervehtijä = new Tervehtijä();
+            tervehtijä.Tervehdi();
+            Console.WriteLine(tervehtijä.HaeTervehdys());
+
+            
+
+
+            
+        }
+    }
+}
