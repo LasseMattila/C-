fc671d09c123ba247ad213702e9614c18607c7f7
LasseMattila <46316152+LasseMattila@users.noreply.github.com>
2019-01-12 17:09:58 +0200

    Add files via upload

---
+++ Tehtävät 29.8.18/Tehtävät 29.8.18/Ohjelma1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laskuri1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double sum = 0, avg = 0;
+
+            Console.Write("Montako lukua?");
+            int n = Int32.Parse(Console.ReadLine());
+            double[] numbers = new double[n];
+
+            for (int i = 0; i < numbers.Length; i++)
+
+            {
+                Console.Write("Anna numero: {0}: ", i + 1);
+                numbers[i] = Convert.ToDouble(Console.ReadLine());
+                sum += numbers[i];
+            }
+
+            avg = sum / numbers.Length;
+
+            Console.WriteLine("Keskiarvo : " + avg);
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Tehtävät 29.8.18/Tehtävät 29.8.18/Ohjelma2.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laskuri1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Yhteen-vai vähennyslasku (+/-)");
+            string teksti = Console.ReadLine();
+
+
+            Console.WriteLine("Anna ensimmäinen luku");
+            var num1 = Console.ReadLine();
+            int luku1 = int.Parse(num1);
+
+            Console.WriteLine("Anna toinen luku");
+            var num2 = Console.ReadLine();
+            int luku2 = int.Parse(num2);
+
+            if (teksti.ToLower().Contains("yhteen"))
+            {
+                int luku3 = luku1 + luku2;
+                Console.WriteLine(luku3);
+            }
+
+            else
+            {
+                int luku3 = luku1 - luku2;
+                Console.WriteLine(luku3);
+            }
+
+            
+
+        }
+    }
+}
+
